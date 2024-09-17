ad33b1eecd3ce84002b32ca566e5049e9b3e7307
晨阳暮鹿 <2209739937@qq.com>
2024-09-17 11:39:29 +0800

    初始

--- sky-common/src/main/java/com/sky/constant/MessageConstant.java
+++ sky-common/src/main/java/com/sky/constant/MessageConstant.java
@@ -4,7 +4,6 @@ package com.sky.constant;
  * 信息提示常量类
  */
 public class MessageConstant {
-
     public static final String PASSWORD_ERROR = "密码错误";
     public static final String ACCOUNT_NOT_FOUND = "账号不存在";
     public static final String ACCOUNT_LOCKED = "账号被锁定";
@@ -23,5 +22,4 @@ public class MessageConstant {
     public static final String DISH_BE_RELATED_BY_SETMEAL = "当前菜品关联了套餐,不能删除";
     public static final String ORDER_STATUS_ERROR = "订单状态错误";
     public static final String ORDER_NOT_FOUND = "订单不存在";
-
 }
