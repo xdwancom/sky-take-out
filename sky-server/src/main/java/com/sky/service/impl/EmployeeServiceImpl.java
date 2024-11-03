d958a2f5939f6720bec383595dac9ae4a4240dff
晨阳暮鹿 <2209739937@qq.com>
2024-11-03 21:20:49 +0800

    部分业务层替换为mybatis-plus

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,16 +1,20 @@
 package com.sky.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,8 +39,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         String username = employeeLoginDTO.getUsername();
         String password = employeeLoginDTO.getPassword();
         password = DigestUtils.md5DigestAsHex(password.getBytes());//md5加密
-
-        Employee employee = employeeMapper.getByUsername(username);//根据用户名查询数据库
+        Employee employee = employeeMapper.selectOne(new QueryWrapper<Employee>().eq("username",username));//根据用户名查询数据库
 
         if (employee == null)
             throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);//账号不存在
@@ -68,4 +71,17 @@ public class EmployeeServiceImpl implements EmployeeService {
         employeeMapper.insert(employee);
     }
 
+    /**
+     * 分页查询
+     *
+     * @param employeePageQueryDTO
+     * @return
+     */
+    public PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO) {
+        // select * from employee limit 0,10
+        //开始分页查询
+        PageHelper.startPage(employeePageQueryDTO.getPage(), employeePageQueryDTO.getPageSize());
+
+        return null;
+    }
 }
